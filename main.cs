f8e18c23aff8695a1c2fbf6053a34a01038d969c
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-02 18:12:35 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -6,9 +6,10 @@ public class Out
     public static void Main(string[] args)
     {
         AES256 Aes256 = new AES256();
-        Console.WriteLine("input what to encrypt using the aes256 encryption method");
-        Console.Write("input: ");
-        string input = Console.ReadLine();
+        // Console.WriteLine("input what to encrypt using the aes256 encryption method");
+        // Console.Write("input: ");
+        // string input = Console.ReadLine();
+        string input = "asdfghjklqwerty";
         Aes256.Encrypt(input);
     }
 }
