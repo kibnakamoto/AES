4f73bf1840d1e50f28b8a2cdd20810786245a062
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-18 22:33:42 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -6,9 +6,21 @@ public class Out
     public static void Main(string[] args)
     {
         AES256 Aes256 = new AES256();
-        Console.WriteLine("input what to encrypt using the aes256 encryption method");
-        Console.Write("\ninput: ");
-        string input = Console.ReadLine();
-        System.Console.WriteLine("\nEnc_AES256: " + Aes256.Encrypt(input));
+        OPS_AES Operation = new OPS_AES();
+        
+        // Console.WriteLine("input what to encrypt using the aes256 encryption method");
+        // Console.Write("\ninput: ");
+        // string input = Console.ReadLine();
+        byte[] key = new byte[32];
+        string input = "asdfghjklqwertyu";
+
+        // key changes based on input
+        key = Operation.CreateKey(input, 32); // uses salt
+        // foreach(char c in key) {
+        //     Console.Write(c);
+        // }
+
+
+        System.Console.WriteLine("\nEnc_AES256: " + Aes256.Encrypt(input, key));
     }
 }
