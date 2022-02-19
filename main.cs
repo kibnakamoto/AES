75a8d5d636d6f9653881c73337709ea6e996304a
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-02-19 08:43:49 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -5,22 +5,19 @@ public class Out
 {
     public static void Main(string[] args)
     {
+        /* works no matter the length of input */
+        
         AES256 Aes256 = new AES256();
         OPS_AES Operation = new OPS_AES();
         
-        // Console.WriteLine("input what to encrypt using the aes256 encryption method");
-        // Console.Write("\ninput: ");
-        // string input = Console.ReadLine();
-        byte[] key = new byte[32];
-        string input = "asdfghjklqwertyu";
-
+        Console.WriteLine("input what to encrypt using the aes256 encryption method");
+        Console.Write("\ninput: ");
+        string input = Console.ReadLine();
+        
         // key changes based on input
+        byte[] key = new byte[32];
         key = Operation.CreateKey(input, 32); // uses salt
-        // foreach(char c in key) {
-        //     Console.Write(c);
-        // }
-
-
+        
         System.Console.WriteLine("\nEnc_AES256: " + Aes256.Encrypt(input, key));
     }
 }
