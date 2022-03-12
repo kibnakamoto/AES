1c61b4a71e6f569e3c1933dfa0f48a5f51964fe4
kibnakamoto <86175813+kibnakamoto@users.noreply.github.com>
2022-03-11 19:23:13 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,16 @@
 
 int main()
 {
-    AES :: AES256 aes256;
+    AES::AES128 aes128;
+    AES::AES192 aes192;
+    AES::AES256 aes256;
     typedef unsigned char uint8_t;
     uint8_t key[32] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
                        0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,
                        0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
-    std::cout << aes256.encrypt("msgsmsgsmsgsmsgs", key);
+    std::string encryptedMsg =  aes256.encrypt("msgsmsgsmsgsmsgsmsgsmsgs", key);
+    std::string decryptedMsg = aes256.decrypt(encryptedMsg, key);
+    std::cout << encryptedMsg << std::endl << std::endl;
+    std::cout << decryptedMsg;
     return 0;
 }
